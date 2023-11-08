b6a4ef38b3e653db879ddb8ab2399be9e0444755
Meetky <meetky@sina.cn>
2023-11-08 23:14:31 +0800

    修改socket内容

--- run_socket.py
+++ run_socket.py
@@ -1,11 +1,34 @@
 import socket
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-sock.bind(("0.0.0.0", 8002))
-sock.listen(5)
+# 创建 socket对象
+socket_server = socket.socket()
 
-conn, address = sock.accept()  # 获取客户端的socket对象和地址
+# 绑定 socket_server到指定的ip地址
+socket_server.bind(("localhost", 8888))
 
-msg = conn.recv(1024)  # 获取客户端的握手信息
-print(msg)
+# 监听端口, listen()内书写数字，表示可以接受链接的数量
+socket_server.listen(1)
+
+while True:
+
+    # 等待客户端连接，接收到的 result是一个二元元组, accept()是一个阻塞的方法，如果没有连接不会往下执行
+    result = socket_server.accept()
+    conn = result[0]  # 客户端连接对象
+    address = result[1]  # 客户端地址信息
+    print(f"接收到的客户端连接信息为{address}")
+    # 接收客户端信息，recv接受的参数是缓冲区大小，一般1024即可，返回的是一个字节数组，bytes对象，不是字符串，再将其decode解码为字符串对象
+    data = conn.recv(1024).decode("UTF-8")
+
+    print(f"客户端发来的消息是:{data}")
+
+    # 回复消息
+    # msg = input("请输入回复的消息:")
+    msg = "hello client!"
+    if msg == 'exit':
+        break
+
+    conn.send(msg.encode("UTF-8"))
+    # 关闭连接
+    conn.close()
+
+socket_server.close()
