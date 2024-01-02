b8e828ba0c5d1cd04769b67c0a900ea85265e9ed
Concon <meetky@sina.cn>
2024-01-02 14:57:08 +0800

    2024-01-02 14:57:08commit

--- run_socket.py
+++ run_socket.py
@@ -1,52 +1,23 @@
-import socket
-
-from socket_demo import return_msg as res
-
-
-class SocketServer():
-    def __init__(self):
-        # 创建 socket对象
-        self.socket_server = socket.socket()
-        # 绑定 socket_server到指定的ip地址
-        self.socket_server.bind(("0.0.0.0", 7071))
-        # 监听端口, listen()内书写数字，表示可以接受链接的数量
-        self.socket_server.listen(1)
-
-    def interactive(self):
-        while True:
-            # 等待客户端连接，接收到的 result是一个二元元组, accept()是一个阻塞的方法，如果没有连接不会往下执行
-            result = self.socket_server.accept()
-            conn = result[0]  # 客户端连接对象
-            address = result[1]  # 客户端地址信息
-            print(f"接收到的客户端连接信息为{address}")
-            # 接收客户端信息，recv接受的参数是缓冲区大小，一般1024即可，返回的是一个字节数组，bytes对象，不是字符串，再将其decode解码为字符串对象
-            data = conn.recv(1024).decode("UTF-8")
-
-            print(f"客户端发来的消息是:{data}")
-            # 回复消息
-            # msg = input("请输入回复的消息:")
-            # msg = "hello client!"
-            msg = res.return_msg(data)
-            if msg == 'exit':
-                break
-
-            conn.send(msg.encode("UTF-8"))
-            print(f"回复内容是：{msg}")
-            # 关闭连接
-            conn.close()
-
-    def main(self):
-        try:
-            self.interactive()
-        except Exception as e:
-            raise e
-        finally:
-            print("连接异常，重新等待连接")
-            return self.main()
-
-    def __del__(self):
-        self.socket_server.close()
+import json
+from socket_demo.return_msg import *
+import asyncio
+import websockets
 
 
-if __name__ == '__main__':
-    SocketServer().main()
+async def echo(websocket, path):
+    while True:
+        message = await websocket.recv()
+        print(message)
+        if message is None:
+            break
+        if message == "2d":
+            websocket.send(json.dumps(resource2d.main()))
+        elif message == "sign":
+            websocket.send(json.dumps(sign.main(3)))
+        else:
+            await websocket.send("Unknown Message")
+
+
+start_server = websockets.serve(echo, "0.0.0.0", 7071)
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
