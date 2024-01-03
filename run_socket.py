3fa9397ac4a55d209994e87f56dc3c7464348555
Concon <meetky@sina.cn>
2024-01-03 15:49:20 +0800

    2024-01-03 15:49:19commit

--- run_socket.py
+++ run_socket.py
@@ -8,6 +8,7 @@ async def echo(websocket, path):
     try:
         while True:
             message = await websocket.recv()
+            print(message)
             if message is None:
                 break
             try:
@@ -15,20 +16,19 @@ async def echo(websocket, path):
                     raise ValueError("Missing 'sub_topic' field")
                 message = json.loads(json.loads(message)["body"])
                 sub_topic = message["sub_topic"]
-                if not isinstance(sub_topic, list):
-                    raise ValueError("Invalid 'sub_topic' format")
-                if sub_topic[0] == "2d":
-                    return_msg = json.dumps(resource2d.main())
-                    await websocket.send(return_msg)
-                elif sub_topic[0] == "sign":
-                    return_msg = json.dumps(sign.main(3))
-                    await websocket.send(return_msg)
+                if sub_topic:
+                    await websocket.send(message)
+                # if sub_topic[0] == "2d":
+                #     return_msg = json.dumps(resource2d.main())
+                #     await websocket.send(return_msg)
+                # elif sub_topic[0] == "sign":
+                #     return_msg = json.dumps(sign.main(3))
+                #     await websocket.send(return_msg)
                 else:
                     await websocket.send("Unknown Message")
             except ValueError as e:
                 print(f"Error processing message: {e}")
                 await websocket.send("Invalid message format")
-            await websocket.send("Client disconnected unexpectedly.")
     except websockets.ConnectionClosedOK:
         await websocket.send("Client disconnected unexpectedly.")
 
